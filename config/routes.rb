2e5bdb8ebdbd08a1ede5f7b7a0767a01b1de4eb1
Ayush <unitedestates.ayush10@gmail.com>
2024-11-17 18:05:08 +0530

    sign up and logout

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   # GET /about
   get "/about", to: "about#index" 
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
+
   # get "/", to: "main#index"
   root to: "main#index"
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
